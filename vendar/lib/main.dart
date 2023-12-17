6457a3dafc43f9ae81230b41bd8cac6784e23108
Ege Ayan <139765763+ege-ayan@users.noreply.github.com>
2023-12-17 22:14:10 +0300

    UI enhanced

--- vendar/lib/main.dart
+++ vendar/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ar_flutter_plugin/ar_flutter_plugin.dart';
 import 'package:flutter/services.dart';
-import 'ARExample.dart';
+import 'item_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,21 +23,14 @@ class _MyAppState extends State<MyApp> {
     initPlatformState();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
     String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
     try {
       platformVersion = await ArFlutterPlugin.platformVersion;
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
     if (!mounted) return;
-
     setState(() {
       _platformVersion = platformVersion;
     });
@@ -66,34 +59,48 @@ class ExampleList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final examples = [
-      Example(
-          'Anchors & Objects on Planes',
-          'Place 3D objects on detected planes using anchors',
-              () => Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => ObjectsOnPlanesWidget()))),
+    // Define your products here
+    final products = [
+      Product(
+        name: 'Table',
+        imageUrl: 'assets/images/Table.png', // Local asset for demonstration
+        price: 35.00,
+        description: 'A sturdy table made of oak wood.',
+        arUrl: 'https://github.com/vendAR-project/vendAR/raw/main/vendar/models/Table/Table.glb',
+      ),
+      // Add more products here
     ];
+
+    final examples = products.map((product) {
+      return Example(
+        product.name,
+        'View Model',
+            () => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ProductDetailView(product: product),
+          ),
+        ),
+      );
+    }).toList();
+
     return ListView(
-      children:
-      examples.map((example) => ExampleCard(example: example)).toList(),
+      children: examples.map((example) => ExampleCard(example: example)).toList(),
     );
   }
 }
 
 class ExampleCard extends StatelessWidget {
-  ExampleCard({Key? key, required this.example}) : super(key: key);
   final Example example;
 
+  ExampleCard({Key? key, required this.example}) : super(key: key);
+
   @override
-  build(BuildContext context) {
+  Widget build(BuildContext context) {
     return Card(
       child: InkWell(
         splashColor: Colors.blue.withAlpha(30),
-        onTap: () {
-          example.onTap();
-        },
+        onTap: () => example.onTap(),
         child: ListTile(
           title: Text(example.name),
           subtitle: Text(example.description),
@@ -104,8 +111,9 @@ class ExampleCard extends StatelessWidget {
 }
 
 class Example {
-  const Example(this.name, this.description, this.onTap);
   final String name;
   final String description;
-  final Function onTap;
+  final VoidCallback onTap;
+
+  Example(this.name, this.description, this.onTap);
 }
