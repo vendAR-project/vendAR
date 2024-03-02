5226880c0c8e8f7e2c86664840e52486d280cb6c
enderutlu <ender.utlu@ug.bilkent.edu.tr>
2024-03-02 20:29:37 +0300

    Schema.sql done

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -1,3 +1,29 @@
-CREATE TABLE IF NOT EXISTS User (
+CREATE TABLE IF NOT EXISTS Person (
+    user_id uuid NOT NULL,
+    user_name char(35) NOT NULL,
+    user_surname char(30) NOT NULL,
+    user_password char(20) NOT NULL,
+    user_email char(60) NOT NULL,
+    user_phone char(60) NOT NULL,
+    PRIMARY KEY(user_id)
+);
 
+CREATE TABLE IF NOT EXISTS Product (
+    product_id uuid NOT NULL,
+    user_id uuid NOT NULL,
+    product_title char(100) NOT NULL,
+    product_desc text,
+    product_images text[],
+    product_features text[],
+    PRIMARY KEY(product_id),
+    FOREIGN KEY(user_id) REFERENCES Person(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS Model (
+    model_id uuid NOT NULL,
+    product_id uuid NOT NULL,
+    model_dimensions float8[] NOT NULL,
+    model_src text NOT NULL,
+    PRIMARY KEY(model_id),
+    FOREIGN KEY(product_id) REFERENCES Product(product_id)
 );
