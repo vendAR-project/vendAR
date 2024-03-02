061b08ef26735cd9d6212947a21c7724a92f44fb
Mehmet Feyyaz Kucuk <mfkucuk555@gmail.com>
2024-03-02 19:28:54 +0300

    spring boot init

---
+++ backend/src/main/java/com/cs491/vendar/VendarApplication.java
@@ -0,0 +1,13 @@
+package com.cs491.vendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendarApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cs491/vendar/VendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs491.vendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
