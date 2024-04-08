64be561c9f3fe5d08d7c7d32dd78dc8f377acc93
Ege Ayan <egeayan2478@gmail.com>
2024-04-08 15:32:51 +0300

    feat: register_view and register_controller connected

--- vendar/lib/components/register/register_view.dart
+++ vendar/lib/components/register/register_view.dart
@@ -1,121 +1,182 @@
 import 'package:flutter/material.dart';
 import 'package:vendar/components/login/login_view.dart';
+import 'package:vendar/components/register/register_controller.dart';
 
-class RegisterView extends StatelessWidget {
+class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
+
+  @override
+  State<RegisterView> createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _surnameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _retypePasswordController =
+      TextEditingController();
+
+  final RegisterController _registerController = RegisterController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(height: 100.0),
-            const SizedBox(
-              height: 80.0,
-              child: Center(
-                child: Text(
-                  'Register',
-                  style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 100.0),
+              const SizedBox(
+                height: 80.0,
+                child: Center(
+                  child: Text(
+                    'Register',
+                    style:
+                        TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 20.0),
-
-            const Row(
-              children: [
-                Flexible(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Name',
-                      border: OutlineInputBorder(),
+              const SizedBox(height: 20.0),
+              Row(
+                children: [
+                  Flexible(
+                    child: TextFormField(
+                      controller: _nameController,
+                      decoration: const InputDecoration(
+                        labelText: 'Name',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your name.';
+                        }
+                        return null;
+                      },
                     ),
                   ),
-                ),
-                SizedBox(width: 10.0),
-                Flexible(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Surname',
-                      border: OutlineInputBorder(),
+                  const SizedBox(width: 10.0),
+                  Flexible(
+                    child: TextFormField(
+                      controller: _surnameController,
+                      decoration: const InputDecoration(
+                        labelText: 'Surname',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your surname.';
+                        }
+                        return null;
+                      },
                     ),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 10.0),
-
-            const SizedBox(
-              height: 50.0,
-              child: TextField(
-                decoration: InputDecoration(
+                ],
+              ),
+              const SizedBox(height: 10.0),
+              TextFormField(
+                controller: _emailController,
+                decoration: const InputDecoration(
                   labelText: 'Email',
                   border: OutlineInputBorder(),
                 ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email address.';
+                  }
+                  return null;
+                },
               ),
-            ),
-            const SizedBox(height: 10.0),
-
-            const SizedBox(
-              height: 50.0,
-              child: TextField(
-                decoration: InputDecoration(
+              const SizedBox(height: 10.0),
+              TextFormField(
+                controller: _passwordController,
+                decoration: const InputDecoration(
                   labelText: 'Password',
                   border: OutlineInputBorder(),
                   suffixIcon: Icon(Icons.visibility_off),
                 ),
                 obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password.';
+                  }
+                  return null;
+                },
               ),
-            ),
-            const SizedBox(height: 10.0),
-
-            const SizedBox(
-              height: 50.0,
-              child: TextField(
-                decoration: InputDecoration(
+              const SizedBox(height: 10.0),
+              TextFormField(
+                controller: _retypePasswordController,
+                decoration: const InputDecoration(
                   labelText: 'Retype Password',
                   border: OutlineInputBorder(),
                   suffixIcon: Icon(Icons.visibility_off),
                 ),
                 obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please retype your password.';
+                  } else if (value != _passwordController.text) {
+                    return 'Passwords do not match.';
+                  }
+                  return null;
+                },
               ),
-            ),
-            const SizedBox(height: 20.0),
-
-            // Register button with spacing
-            SizedBox(
-              height: 75.0,
-              child: Center(
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: Size(MediaQuery.of(context).size.width, 50),
+              const SizedBox(height: 20.0),
+              SizedBox(
+                height: 75.0,
+                child: Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        String name = _nameController.text;
+                        String surname = _surnameController.text;
+                        String email = _emailController.text;
+                        String password = _passwordController.text;
+                        _registerController.register(
+                            name, surname, email, password);
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(MediaQuery.of(context).size.width, 50),
+                    ),
+                    child: const Text('Register'),
                   ),
-                  child: const Text('Register'),
                 ),
               ),
-            ),
-            const SizedBox(height: 10.0),
-            SizedBox(
-              height: 50.0,
-              child: Center(
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const LoginView()),
-                    );
-                  },
-                  child: const Text('Go back to Login Screen'),
+              const SizedBox(height: 10.0),
+              SizedBox(
+                height: 50.0,
+                child: Center(
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const LoginView()),
+                      );
+                    },
+                    child: const Text('Go back to Login Screen'),
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _surnameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _retypePasswordController.dispose();
+    super.dispose();
   }
 }
