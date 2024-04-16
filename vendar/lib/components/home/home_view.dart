9ff57c0a9d1927aebac503fa7bcfa6e144efd117
Ege Ayan <egeayan2478@gmail.com>
2024-04-16 15:07:42 +0300

    feat: main page divided into marketplace and favourites

--- vendar/lib/components/home/home_view.dart
+++ vendar/lib/components/home/home_view.dart
@@ -5,6 +5,34 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Home Screen'));
+    return DefaultTabController(
+      length: 2, // Total number of tabs
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: const TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.shopping_cart),
+                text: 'Marketplace',
+              ),
+              Tab(
+                icon: Icon(Icons.recommend),
+                text: 'Recommended',
+              ),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: <Widget>[
+            Center(
+              child: Text('Marketplace View'),
+            ),
+            Center(
+              child: Text('Recommended View'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
