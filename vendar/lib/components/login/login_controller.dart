cb1d5fc956b260d542e0defc9a13fc5c67639123
Ege Ayan <egeayan2478@gmail.com>
2024-04-25 20:05:22 +0300

    feat: login controller connected to the backend

--- vendar/lib/components/login/login_controller.dart
+++ vendar/lib/components/login/login_controller.dart
@@ -1,3 +1,22 @@
+import 'package:dio/dio.dart';
+
 class LoginController {
-  Future<void> login(String email, String password) async {}
+  Dio dio = Dio();
+
+  Future<void> login(String email, String password) async {
+    final String url = "http://54.93.83.196:8080/login";
+    final Map<String, dynamic> body = {
+      "user_email": email,
+      "user_password": password
+    };
+
+    try {
+      Response response = await dio.post(url, data: body);
+      // Handle the response as necessary
+      print('Login Successful: ${response.data}');
+    } catch (e) {
+      // Handle errors or exceptions
+      print('Error occurred: $e');
+    }
+  }
 }
