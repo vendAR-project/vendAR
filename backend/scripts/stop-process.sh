c46af14e72a54dc94d5ad314684cd32d2f4b701d
enderutlu <85748258+enderutlu@users.noreply.github.com>
2024-04-28 01:11:12 +0300

    Update stop-process.sh

--- backend/scripts/stop-process.sh
+++ backend/scripts/stop-process.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 sudo chmod +x stop-process.sh
-ps -ef | grep vendar-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{print $2}' | xargs kill
+# Check if any processes are running before attempting to kill them
+if pgrep -f "java -jar vendar-0.0.1-SNAPSHOT.jar" > /dev/null; then
+    # Processes are running, so proceed to kill them
+    
+    ps -ef | grep vendar-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{print $2}' | xargs kill
+else
+    # No processes are running, so no need to kill them
+    echo "No processes found to kill. Deployment will proceed."
+fi
