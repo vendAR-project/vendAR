b5f34f852865595d11f9f2b9ab8188b04e93e76c
enderutlu <85748258+enderutlu@users.noreply.github.com>
2024-04-28 16:03:52 +0300

    Update start-process.sh

--- backend/scripts/start-process.sh
+++ backend/scripts/start-process.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo nohup java -jar /home/ec2-user/vendar-0.0.1-SNAPSHOT.jar > /vendAR/application.log 2>&1 &
+sudo nohup java -jar /home/ec2-user/vendar-0.0.1-SNAPSHOT.jar > /home/ec2-user/application.log 2>&1 &
